2c21c7d53f8746106420d02f7a93f7dd4c71a2bd
Flavio Colonna <flavio.colonna@ofertia.com>
2021-03-10 19:32:46 +0100

    Add prod and debug mode

--- gulpfile.js/jsTasks.js
+++ gulpfile.js/jsTasks.js
@@ -2,6 +2,10 @@ const gulp = require('gulp');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
+const args = require('yargs').argv;
 const paths = require('./paths');
 
 const browserifyBundle = function (entry) {
@@ -13,10 +17,19 @@ const browserifyBundle = function (entry) {
         .pipe(buffer());
 }
 const createJSBundle = function () {
+    const prod = args.prod;
+    const debug = args.debug;
     return browserifyBundle(paths.getJSEntry())
+        .pipe(gulpIf(debug, sourcemaps.init()))
+        .pipe(gulpIf(prod, uglify()))
+        .pipe(gulpIf(debug, sourcemaps.write('./')))    
         .pipe(gulp.dest(paths.getJSOutputFolder()));
 }
 const watchJS = function(cb) {
+    const prod = args.prod;
+    if(prod) {
+        return cb();
+    }
     gulp.watch(paths.getJSEntryFolder(), createJSBundle);
     cb();
 }
