5c3fe481f9b319f4b2bb324f37af4e53d16d6a96
Flavio Colonna <flavio.colonna@ofertia.com>
2021-03-11 07:17:27 +0100

    Update unit tests

--- jest/mocks/modelExamples.js
+++ jest/mocks/modelExamples.js
@@ -8,7 +8,19 @@ const NOT_VALID_EXPENSE_OPERATION = {
     description: 'Bill',
     type: 'EXPENSE'
 }
+const INCOME_OPERATION = {
+    amount: 1000,
+    description: 'Salary',
+    type: 'INCOME'
+}
+const NOT_VALID_INCOME_OPERATION = {
+    amount: 100,
+    description: 'Bill',
+    type: 'EXPENSE'
+}
 module.exports = {
     EXPENSE_OPERATION: EXPENSE_OPERATION,
+    INCOME_OPERATION: INCOME_OPERATION,
+    NOT_VALID_INCOME_OPERATION: NOT_VALID_INCOME_OPERATION,
     NOT_VALID_EXPENSE_OPERATION: NOT_VALID_EXPENSE_OPERATION
 }
--- src/js/models/Wallet.spec.js
+++ src/js/models/Wallet.spec.js
@@ -1,28 +1,67 @@
-const Wallet = require('./Wallet');
-const ModelExamples = require('../../../jest/mocks/modelExamples');
+const Wallet = require("./Wallet");
+const Enums = require("../enums");
+const mockedStructures = require("../../../jest/mocks/modelExamples");
 
-describe('Wallet: testing suite', function () {
-    it('Wallet: instance must contain the correct initial state', function () {
+describe("Wallet testing suite", function () {
+    it("First instance should be an empty Wallet", function () {
         const wallet = new Wallet();
         expect(wallet.getBalance()).toBe(0);
         expect(wallet.getOperations().length).toBe(0);
     });
-    it('Wallet: add valid expense operation', function () {
+    it("addOperation: it works with an income operation", function () {
         const wallet = new Wallet();
-        const operation = ModelExamples.EXPENSE_OPERATION;
+        const operation = mockedStructures.INCOME_OPERATION;
         wallet.addOperation(operation);
-        const operationAdded = wallet.getOperations()[0];
-        expect(wallet.getBalance()).toBe(-100);
-        expect(operationAdded.description).toBe(operation.description);
+        expect(wallet.getBalance()).toBe(operation.amount);
         expect(wallet.getOperations().length).toBe(1);
     });
-    it('Wallet: add invalid expense operation', function () {
+    it("addOperation: it works with an outcome operation", function () {
         const wallet = new Wallet();
-        const operation = ModelExamples.NOT_VALID_EXPENSE_OPERATION;
+        const operation = mockedStructures.EXPENSE_OPERATION;
+        wallet.addOperation(operation);
+        expect(wallet.getBalance()).toBe(-operation.amount);
+        expect(wallet.getOperations().length).toBe(1);
+    });
+    it("addOperation: it fires an error when adding an invalid operation", function () {
+        const wallet = new Wallet();
         try {
-            wallet.addOperation(operation);
-        } catch (e) {
-            expect(e.message).toBe('INVALID_OPERATION');
+            wallet.addOperation(mockedStructures.NOT_VALID_EXPENSE_OPERATION);
+        } catch(e) {
+            expect(e.message).toBe(Enums.WalletErrors.INVALID_OPERATION);
         }
+    });
+    it("removeOperation: it works removing an income operation", function () {
+        const wallet = new Wallet();
+        const operation = mockedStructures.INCOME_OPERATION;
+        wallet.addOperation(operation);
+        const lastOperationAdded = wallet.getOperations()[0];
+        wallet.removeOperation(lastOperationAdded.id);
+        expect(wallet.getBalance()).toBe(0);
+        expect(wallet.getOperations().length).toBe(0);
+    });
+    it("removeOperation: it works removing an outcome operation", function () {
+        const wallet = new Wallet();
+        const operation = mockedStructures.EXPENSE_OPERATION;
+        wallet.addOperation(operation);
+        const lastOperationAdded = wallet.getOperations()[0];
+        wallet.removeOperation(lastOperationAdded.id);
+        expect(wallet.getBalance()).toBe(0);
+        expect(wallet.getOperations().length).toBe(0);
+    });
+    it("removeOperation: it fires the correct error when date/id not found", function () {
+        const wallet = new Wallet();
+        try {
+            wallet.removeOperation(242389239);
+        } catch(e) {
+            expect(e.message).toBe(Enums.WalletErrors.OPERATION_NOT_FOUND);
+        }
+    });
+    it("findOperation: it works finding a correct operation passing a search value", function () {
+        const wallet = new Wallet();
+        const operation = mockedStructures.INCOME_OPERATION;
+        wallet.addOperation(operation);
+        const searchValue = operation.description.substring(0, 2);
+        const operationsFound = wallet.findOperations(searchValue);
+        expect(operationsFound.length).toBe(1);
     });
 });
