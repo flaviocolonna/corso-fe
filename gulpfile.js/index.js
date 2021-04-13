d51d208037965aa7e3c66f11ccb7b30f061d6116
Flavio Colonna <flavio.colonna@ofertia.com>
2021-04-13 16:20:13 +0200

    Add css, html and icons to define page layout

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,14 +2,19 @@ const gulp = require('gulp');
 const del = require('del');
 const viewTasks = require('./viewTasks');
 const jsTasks = require('./jsTasks');
+const assetsTasks = require('./assetsTasks');
 const paths = require('./paths');
 const serve = require('./serveTasks');
 
-const clean = function(cb) {
+const clean = function (cb) {
     del.sync(paths.getDistFolder(), { force: true });
     cb();
 }
-const build = gulp.series(clean, viewTasks.compileIndex, jsTasks.createJSBundle, jsTasks.watchJS, serve);
+const build = gulp.series(clean,
+    assetsTasks.processIcons,
+    assetsTasks.watchIcons,
+    assetsTasks.processCSS,
+    assetsTasks.watchCSS, viewTasks.compileIndex, jsTasks.createJSBundle, jsTasks.watchJS, serve);
 
 module.exports = {
     build: build,
--- gulpfile.js/viewTasks.js
+++ gulpfile.js/viewTasks.js
@@ -4,7 +4,9 @@ const paths = require('./paths');
 
 const compileIndex = function () {
     const jsIndex = gulp.src(paths.getJSEntry());
+    const cssIndex = gulp.src(paths.getCSSEntryPath());
     return gulp.src(paths.getHtmlEntry())
+        .pipe(gulpInject(cssIndex, { relative: true, name: "custom" }))
         .pipe(gulpInject(jsIndex, { relative: true, name: 'custom' }))
         .pipe(gulp.dest(paths.getDistFolder()));
 }
