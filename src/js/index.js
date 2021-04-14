0e073122f2fad1d020793a17f5ba826bdedbc3fd
Flavio Colonna <flavio.colonna@ofertia.com>
2021-04-14 19:45:37 +0200

    Add toggle modal functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
 const Wallet = require('./models/Wallet');
 
 const wallet = new Wallet();
-console.log(wallet.getBalance());
+
+const toggleModal = () => {
+    const modalElmnt = document.getElementById('modal');
+    if(!modalElmnt) {
+        return;
+    }
+    const isHidden = modalElmnt.classList.contains('hide');
+    if(isHidden) {
+        modalElmnt.classList.remove('hide');
+        return;
+    }
+    modalElmnt.classList.add('hide');
+}
+
+window.toggleModal = toggleModal;
